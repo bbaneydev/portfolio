38e74bae8f98d814ebe5e42ba336cfe3b25aacef
Billy Baney <billybaneydev@gmail.com>
2021-09-19 21:17:10 -0500

    made some styling changes

--- src/components/About.js
+++ src/components/About.js
@@ -25,14 +25,14 @@ export default function About() {
     if (!author) return <div>Loading...</div>
 
     return (
-        <main className='relative'>
-            <img src={moon} alt='moon' className='absolute w-full' />
+        <main id="about" className='bg-blue-100'>
+            {/* <img src={moon} alt='moon' className='absolute w-full' /> */}
             <div className='p-10 lg:pt-48 container mx-auto relative'>
-                <section className='bg-blue-800 rounded-lg shadow-2xl lg:flex p-20'>
+                <section className='bg-blue-600 rounded-lg shadow-2xl lg:flex p-20'>
                     <img src={urlFor(author.authorImage).url()} className='rounded w-32 h-32 lg:w-64 lg:h-64 mr-8' alt={author.name}/>
                     <div className='text-lg flex flex-col justify-center'>
-                        <h1 className='cursive text-6xl text-blue-300 mb-4'>Hey there. I'm {" "}<span className='text-blue-100'>{author.name}</span></h1>
-                        <div className='prose lg:prose-xl text-white'>
+                        <h1 className='cursive text-6xl text-black-300 mb-4'>Hey there. I'm {" "}<span className='text-blue-100'>{author.name}</span></h1>
+                        <div className='prose lg:prose-xl text-black'>
                             <BlockContent blocks={author.bio} projectId="w79szajx" dataset='production'/>
                         </div>
                     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import image from '../moon.jpg'
+// import image from '../moon.jpg'
 
 export default function Home() {
     return (
-        <main>
-            <img src={image} alt="moon" className="absolute object-cover w-full h-full" />
+        <main className="absolute object-cover bg-blue-200 w-full h-full">
+            {/* // <img src={image} alt="moon" className="absolute object-cover w-full h-full" /> */}
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-blue-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Billy.</h1>
+                <h1 className="text-6xl text-black-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Billy.</h1>
             </section>
         </main>
     )
